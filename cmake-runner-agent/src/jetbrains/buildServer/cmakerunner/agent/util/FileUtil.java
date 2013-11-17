9bc5a65f6f2bf9c4d9a246110ebc89ac1125efe2
Vladislav Rassokhin <Vladislav.Rassokhin@jetbrains.com>
2013-11-17 19:42:28 +0400

    FileUtil should extends TC openapi FileUtil (cherry picked from commit 39ce08c) Fixed compilation

--- cmake-runner-agent/src/jetbrains/buildServer/cmakerunner/agent/util/FileUtil.java
+++ cmake-runner-agent/src/jetbrains/buildServer/cmakerunner/agent/util/FileUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ import java.util.StringTokenizer;
 /**
  * @author Roman.Chernyatchik
  */
-public class FileUtil {
+public class FileUtil extends jetbrains.buildServer.util.FileUtil {
 
   /**
    * @param path Path to check
