254d2c3874a961ef1d96b12d3a8e01c8005921d0
Vladislav Rassokhin <Vladislav.Rassokhin@jetbrains.com>
2013-11-26 22:58:35 +0400

    Fix FileUtil stack overflow problem (cherry picked from commit 4f6187f)

--- cmake-runner-agent/src/jetbrains/buildServer/cmakerunner/agent/util/FileUtil.java
+++ cmake-runner-agent/src/jetbrains/buildServer/cmakerunner/agent/util/FileUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright 2000-2013 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,11 +54,13 @@ public class FileUtil {
       }
     }
 
-    if (!exeName.endsWith(".exe") && SystemInfo.isWindows) {
-      return findExecutableByNameInPATH(exeName + ".exe", environment);
-    }
-    if (!exeName.endsWith(".bat") && SystemInfo.isWindows) {
-      return findExecutableByNameInPATH(exeName + ".bat", environment);
+    if (SystemInfo.isWindows && !exeName.endsWith(".exe") && !exeName.endsWith(".bat")) {
+      String deep;
+      deep = findExecutableByNameInPATH(exeName + ".exe", environment);
+      if (deep == null) {
+        deep = findExecutableByNameInPATH(exeName + ".bat", environment);
+      }
+      return deep;
     }
     return null;
   }
