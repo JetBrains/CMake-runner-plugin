52333d872f4b1d927e2f69f7254d7126405ef496
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2015-07-16 14:38:51 +0300

    Add one more test

--- cmake-runner-test/src/jetbrains/buildServer/cmakerunner/agent/util/OSUtilTest.java
+++ cmake-runner-test/src/jetbrains/buildServer/cmakerunner/agent/util/OSUtilTest.java
@@ -16,7 +16,9 @@
 
 package jetbrains.buildServer.cmakerunner.agent.util;
 
+import com.intellij.openapi.util.SystemInfo;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class OSUtilTest {
@@ -31,5 +33,17 @@ public class OSUtilTest {
     final String path = OSUtil.getCLIFullPath(System.getenv());
     Assert.assertNotNull(path);
     Assert.assertTrue(FileUtil.checkIfExists(path));
+  }
+
+  @DataProvider(name = "UnixOnly")
+  public static Object[][] UnixOnly() {
+    if (SystemInfo.isUnix) return new Object[][]{{true}};
+    return new Object[][]{};
+  }
+
+  @Test(dataProvider = "UnixOnly")
+  public void testGuessUnixShell(final Boolean unix) throws Exception {
+    final String shell = OSUtil.guessUnixShell(System.getenv());
+    Assert.assertNotNull(shell, "Shell should be guessed on unix machines");
   }
 }
